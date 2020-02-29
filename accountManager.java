9eea1e2325bc736e79e354095c557aadca9c2e1a
Ben <benblackk95@gmail.com>
2020-02-29 13:53:48 -0500

    Added sqlite jar. Example communication with the db for our app

--- accountManager.java
+++ accountManager.java
@@ -1,6 +1,75 @@
 /* This file contains functions that deal with retrieving and storing username and password sets for an account. This file deals with the database. */
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//following this tutorial: https://www.sqlitetutorial.net/sqlite-java/
 
 class AccountManager{
+	
+	private String dbLocation = "";
+	
+	public AccountManager(String dbLocation)
+	{
+		this.dbLocation = "jdbc:sqlite:" + dbLocation;
+	}
+	
+	private Connection getConnection() {
+        // SQLite connection string
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(this.dbLocation);
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+	
+	//For testing...
+	public void queryAllUsers()
+	{
+		
+	}
+	
+	//For testing...
+	public void queryAllUserData()
+	{
+		String sql = "SELECT * FROM UserData";
+        
+        try (Connection conn = getConnection();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("ID") +  "\t" + 
+                                   rs.getString("AccountName") + "\t" +
+                                   rs.getString("Password") + "\t" +
+                                   rs.getInt("UserID") );
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+	}
+	
+	public void addUserData(String AccountName, String Password, int UserID)
+	{
+		String sql = "INSERT INTO UserData(AccountName,Password,UserID) VALUES(?,?,?)";
+		 
+        try (Connection conn = getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, AccountName);
+            pstmt.setString(2, Password);
+            pstmt.setInt(3, UserID);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+	}
+	
 }
--- passwordManager.java
+++ passwordManager.java
@@ -3,5 +3,17 @@
 class PasswordManager{
 	public static void main(String args[]){
 		System.out.println("Hello, World");
+		
+		AccountManager ac = new AccountManager("Db/passwordManagerDb.db"); 
+
+		ac.queryAllUserData();
+		
+		System.out.println();
+		System.out.println("Adding some new data...");
+		
+		ac.addUserData("Amazon", "Pa$$word123", 1);
+		
+		ac.queryAllUserData();
+		//should probably have some close connection here maybe..?
 	}
 }
