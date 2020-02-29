ea07023d7cc78fbed995bfd0c885704e46a60742
Snigdha Tiwari <snigdhatiwari@Snig.local>
2020-02-29 14:52:14 -0500

    User I/O for passwordManager. Asks for who the password is for and password

--- passwordManager.java
+++ passwordManager.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 /* This is the main file for the program with the main(). */
 
 class PasswordManager{
 	public static void main(String args[]){
-		System.out.println("Hello, World");
-		
+		Scanner in = new Scanner(System.in);
+		//Get what the password is for
+		System.out.println("Enter what password is for: ");
+		String username = in.nextLine();
+		//if line is blank
+		while(username.equals("")){
+			System.out.println("Enter what password is for: ");
+			username = in.nextLine();
+		}
+
+		//Get password
+		System.out.println("Enter password: ");
+		String password = in.nextLine();
+		//if line is blank
+		while(password.equals("")){
+			System.out.println("Enter password: ");
+			password = in.nextLine();
+		}
+
 		AccountManager ac = new AccountManager("Db/passwordManagerDb.db"); 
 
 		ac.queryAllUserData();
@@ -14,6 +33,7 @@ class PasswordManager{
 		ac.addUserData("Amazon", "Pa$$word123", 1);
 		
 		ac.queryAllUserData();
-		
+		in.close();
 	}
+	
 }
